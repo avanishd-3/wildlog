74fd7fc4a9c429d8dd18397cdf299265a559d806
Avanish Davuluri <avanishdavuluri@gmail.com>
2026-01-29 12:09:40 -0800

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,25 @@
+import { db } from "@wildlog/db";
+import * as schema from "@wildlog/db/schema/auth";
+import { env } from "@wildlog/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [env.CORS_ORIGIN],
+  emailAndPassword: {
+    enabled: true,
+  },
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [],
+});
