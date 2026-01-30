45caa5487850ce877d25a85d5a3872ce5e358af6
Avanish Davuluri <avanishdavuluri@gmail.com>
2026-01-29 17:19:43 -0800

    add xcode setup

---
+++ apps/ios/wildlog/wildlogTests/wildlogTests.swift
@@ -0,0 +1,17 @@
+//
+//  wildlogTests.swift
+//  wildlogTests
+//
+//  Created by Avanish Davuluri on 1/29/26.
+//
+
+import Testing
+@testable import wildlog
+
+struct wildlogTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
