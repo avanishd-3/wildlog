13316b679555f1402b13a835ccd2843d3b21536c
Avanish Davuluri <avanishdavuluri@gmail.com>
2026-01-29 17:09:59 -0800

    add pothos for building graph ql schemas

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,30 +1,24 @@
 import Fastify from "fastify";
 import mercurius from "mercurius";
+import { apiSchema } from "./builder";
 
 const app = Fastify({
   logger: true,
 });
 
-const schema = `
-  type Query {
-    add(x: Int, y: Int): Int
-  }
-`;
-
-const resolvers = {
-  Query: {
-    add: async (_: any, { x, y }: any) => x + y,
-  },
-};
-
 app.register(mercurius, {
-  schema,
-  resolvers,
-});
-
-app.get("/", async function (_req, reply) {
-  const query = "{ add(x: 2, y: 2) }";
-  return reply.graphql(query);
+  schema: apiSchema,
+  graphiql: true, // Enable GraphQL UI
 });
 
 app.listen({ port: 3000 });
+
+// Route get requests to the GraphiQL endpoint so loading the root URL doesn't show an error
+app.get("/", async (_request, reply) => {
+  reply.redirect("/graphiql");
+});
+
+// Route to Neo4j Browser for convenience
+app.get("/graphdb", async (_request, reply) => {
+  reply.redirect("http://localhost:7474");
+});
