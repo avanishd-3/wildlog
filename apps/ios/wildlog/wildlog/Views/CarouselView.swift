037c88c60b1953b4b976ecad432c6c03423dda6a
Avanish Davuluri <avanishdavuluri@gmail.com>
2026-02-02 12:18:38 -0800

    feat: add carousel view

---
+++ apps/ios/wildlog/wildlog/Views/CarouselView.swift
@@ -0,0 +1,37 @@
+//
+//  CarouselView.swift
+//  WildLog
+//
+//  Created by Avanish Davuluri on 2/2/26.
+//
+
+import SwiftUI
+
+struct CarouselView: View {
+    private let sampleTrips = [
+        "acadia",
+        "yosemite",
+        "zion",
+        "canyonlands",
+        "glacier",
+        "bryce"
+    ]
+    var body: some View {
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 0) {
+                ForEach(sampleTrips, id: \.self) { trip in
+                    Image(trip)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 150, height: 300)
+                        .clipShape(RoundedRectangle(cornerRadius: 16))
+                        .padding(.horizontal, 10)
+                }
+            }
+        }.scrollTargetBehavior(.paging) // Requires iOS 17
+    }
+}
+
+#Preview {
+    CarouselView()
+}
