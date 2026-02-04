140776fcf9e813e5eb0f817ebe6dd18108b4ae76
Derek Cao <derekcao@Dereks-MacBook-Air.local>
2026-02-04 03:10:48 -0800

    Add basic implementation toProfileView that leads to settings , ReviewView with tabs, and SettingsView

--- apps/ios/wildlog/wildlog/Views/ProfileView.swift
+++ apps/ios/wildlog/wildlog/Views/ProfileView.swift
@@ -9,12 +9,44 @@ import SwiftUI
 
 struct ProfileView: View {
     var body: some View {
-        VStack {
-            Spacer()
-            Text("Profile View")
-                .font(.largeTitle)
-            Spacer()
-        }.frame(maxWidth: .infinity)
+        NavigationStack {
+            VStack {
+                Text("Username")
+                Circle()
+                    .fill(Color(UIColor.tertiarySystemGroupedBackground))
+                    .frame(width: 100, height: 100)
+                    .overlay(
+                        Image(systemName: "person.fill")
+                            .font(.system(size: 50))
+                            .foregroundColor(.secondary)
+                    )
+                
+                Text("Bio goes here")
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Favorite Parks")
+                        .padding(.leading, 10)
+                    CarouselView()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                VStack(alignment: .leading, spacing: 5) {
+                    Text("Recent Activity")
+                        .padding(.leading, 10)
+                    CarouselView()
+                }
+                .frame(maxWidth: .infinity, alignment: .leading)
+                
+                Spacer()
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    NavigationLink(destination: SettingsView()) {
+                        Image(systemName: "gearshape")
+                    }
+                }
+            }
+        }
     }
 }
 
