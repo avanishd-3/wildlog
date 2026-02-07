05dee963fbab70923d244f3d7f03bbc27ae31038
Avanish Davuluri <avanishdavuluri@gmail.com>
2026-02-07 13:20:41 -0800

    ui: add animation to pitch toggle button

--- apps/ios/wildlog/wildlog/Components/Map/CustomPitchButton.swift
+++ apps/ios/wildlog/wildlog/Components/Map/CustomPitchButton.swift
@@ -33,6 +33,47 @@ final class CustomPitchButton: UIButton {
 
     func update(for pitch: CGFloat) {
         debugPrint("PitchButton: update(\(pitch))")
-        setTitle(pitch == 0 ? "3D" : "2D", for: .normal)
+        animateTitleChange(to: pitch == 0 ? "3D" : "2D")
+    }
+    
+    func animateTitleChange(to newTitle: String) {
+        // Old title slides up and fades out and new title sides in from bottom
+        // Make it very similar to Apple Maps
+        
+        // Make sure a title exists, which it should
+        guard let label = titleLabel else {
+            setTitle(newTitle, for: .normal)
+            return
+        }
+        
+        let snapshot = UILabel(frame: label.convert(label.bounds, to: self))
+        snapshot.font = label.font
+        snapshot.textColor = label.textColor
+        snapshot.textAlignment = label.textAlignment
+        snapshot.text = label.text
+        snapshot.backgroundColor = .clear
+        snapshot.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(snapshot)
+        
+        // Old text moving out
+        UIView.animate(withDuration: 0.25) {
+            snapshot.alpha = 0
+            snapshot.frame.origin.y += snapshot.bounds.height
+        } completion: { _ in
+            snapshot.removeFromSuperview()
+        }
+        
+        // New text moving in (hide until animation starts)
+        setTitle(newTitle, for: .normal)
+        label.alpha = 0
+        
+        UIView.animate(withDuration: 0.25, delay: 0.2) {
+            label.frame.origin.y -= label.bounds.height
+            label.alpha = 1
+        }
+        
+        
+        
     }
 }
